15ca2d65fa8cb4842fa4e1679a590eb249d072f0
Nebiyyeh Dagdeviren <nebiyyehdagdeviren@gmail.com>
2022-12-30 15:41:11 -0600

    front and back end completed MongoDB connected

--- note-taker-backend/server.js
+++ note-taker-backend/server.js
@@ -12,3 +12,12 @@ app.use(express.json());
 //connect to mongoose
 mongoose.connect("mongodb://localhost:27017/noteDB")
 
+
+//require route
+app.use("/", require("./routes/noteRoute"))
+//after above step it is time to send data from take a note page to our route then to our server and then to mongoDB
+
+app.listen(3001, function(){
+    console.log("express server is running on port 3001");
+})
+
